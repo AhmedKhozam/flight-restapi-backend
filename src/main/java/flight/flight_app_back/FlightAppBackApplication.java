a820e1a72b411fe1acc48031fc2e8cad5dfdd891
ahmedkhozam <ahmedkhozam8085@gamil.com>
2024-06-29 22:07:23 +0300

    set up

---
+++ src/main/java/flight/flight_app_back/FlightAppBackApplication.java
@@ -0,0 +1,13 @@
+package flight.flight_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAppBackApplication.class, args);
+	}
+
+}
